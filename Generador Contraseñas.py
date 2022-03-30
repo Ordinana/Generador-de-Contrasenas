aeb523803b23b07a6a7241b4155c500c0ca2bab1
Carlos Ordiñana Caballer <carlosoa34@gmail.com>
2022-03-31 00:37:23 +0200

    Commit generador de contraseñas sencillo

---
+++ Generador Contraseñas.py
@@ -0,0 +1,54 @@
+import random
+
+print("GENERADOR DE CONTRASEÑAS")
+
+facil = "abcdefghijklmnñopqrstuvwxyzABCDEFGHIJKLMNÑOPQRSTUVWXYZ"
+media = "abcdefghijklmnñopqrstuvwxyzABCDEFGHIJKLMNÑOPQRSTUVWXYZª!@·$%&/()=?¿"
+dificil = "abcdefghijklmnñopqrstuvwxyzABCDEFGHIJKLMNÑOPQRSTUVWXYZª!@·$%&/()=?¿0123456789"
+
+while True:
+    numeros = input("¿Cuántas contraseñas desea generar?: ")
+    numeros = int(numeros)
+
+    longitud = input("¿Cuántos carácteres para cada una?: ")
+    longitud = int(longitud)
+
+    grado = input("¿Qué nivel de seguridad quieres que tenga?\n"
+                "Fácil: F\n"
+                "Media: M\n"
+                "Dificil: D\n"
+                "")
+
+    grado = grado.lower()
+
+    print("\nAquí están tus contraseñas:\n")
+
+    if grado == "f":
+        for pwd in range(numeros):
+            contraseñas = ""
+            for c in range(longitud):
+                contraseñas += random.choice(facil)
+            print(contraseñas)
+        
+
+    if grado == "m":
+        for pwd in range(numeros):
+            contraseñas = ""
+            for c in range(longitud):
+                contraseñas += random.choice(media)
+            print(contraseñas)
+            
+
+    if grado == "d":
+        for pwd in range(numeros):
+            contraseñas = ""
+            for c in range(longitud):
+                contraseñas += random.choice(dificil)
+            print(contraseñas)
+                
+    print()
+    otra = input("¿Desea nuevas contraseñas? (si/no): ")
+    if otra == "si":
+        pass
+    else:
+        break
